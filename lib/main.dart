b4757714b7c9d79ef5023648d32ae4af9ab5da64
Himanshu <himjan0@gmail.com>
2023-03-06 18:39:10 +0530

    [seperatePages]

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,17 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
   BMICalculator({super.key});
 
-  final MaterialColor customPrimaryColor = MaterialColor(
-    0xFF0A0E21,
-    <int, Color>{
-      50: Color(0xFFE3F2FD),
-      100: Color(0xFFBBDEFB),
-      200: Color(0xFF90CAF9),
-      300: Color(0xFF64B5F6),
-      400: Color(0xFF42A5F5),
-      500: Color(0xFF2196F3),
-      600: Color(0xFF1E88E5),
-      700: Color(0xFF1976D2),
-      800: Color(0xFF1565C0),
-      900: Color(0xFF0D47A1),
-    },
-  );
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: const Color(0xFF0A0E21),
-        ),
-        scaffoldBackgroundColor: Color.fromRGBO(10, 14, 33, 1.000),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
       home: InputPage(),
     );
@@ -36,29 +19,4 @@ class BMICalculator extends StatelessWidget {
 }
 //Color.fromARGB(1, 10, 14, 34)
 //Color(0x0A0E21),
-
-class InputPage extends StatefulWidget {
-  const InputPage({super.key});
-
-  @override
-  InputPageState createState() => InputPageState();
-}
-
-class InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
+//Color.fromRGBO(10, 14, 33, 1.000),
